ae8d4258a62c1987fdfe8f9023775c7184066e70
jihyun <gkang0702@gmail.com>
2025-05-01 00:03:12 +0900

    Update Addition Table.cpp
    Update malfunction of doubling, add scalar multiplication and check cyclic

--- Addition Table.cpp
+++ Addition Table.cpp
@@ -10,7 +10,7 @@ typedef struct _Point {
 }Point;
 
 // Inverse table of each elements in GF(2^4)
-// 몇 번째에 해당 원소의 역원이 나오는지
+// 몇 번째 index에 해당 원소의 역원이 나오는지
 int gf_inverse[16] = {
     -1, // 0 has no inverse
     1,  // 1
@@ -33,7 +33,7 @@ int gf_inverse[16] = {
 // GF(2^4) multiplication
 int gf_mul(int a, int b) {
     int result = 0;
-    int modulo = 0b10011;  // x^4 + x + 1
+    int modulo = 0b10011;  // Reduction polynomial: x^4 + x + 1
 
     for (int i = 0; i < 4; i++) {
         if ((b >> i) & 1) {
@@ -53,29 +53,29 @@ int gf_mul(int a, int b) {
 
 // Define Point Doubling
 Point doubling(Point P1) {
-    Point R;
+    Point P3;
 
     if (P1.x == 0) {
         // P_1 * 2 = Point of infinity when P1.x = 0
-        R.x = 0;
-        R.y = 0;
-        return R;
+        P3.x = 0;
+        P3.y = 0;
+        return P3;
     }
     else {
-        int plus = P1.x ^ P1.y;
-        int inverse = gf_inverse[P1.x];
-        int lambda = gf_mul(plus, inverse);
+        int inv = gf_inverse[P1.x];
+        int frac = gf_mul(P1.y, inv);
+        int lambda = P1.x ^ frac;
 
-        R.x = (gf_mul(lambda, lambda)) ^ lambda ^ A;
-        R.y = (gf_mul(P1.x, P1.x)) ^ gf_mul(lambda, R.x) ^ R.x;
+        P3.x = (gf_mul(lambda, lambda)) ^ lambda ^ A;
+        P3.y = (gf_mul(P1.x, P1.x)) ^ gf_mul(lambda, P3.x) ^ P3.x;
     }
 
-    return R;
+    return P3;
 }
 
 // Define Point Addition
 Point addition(Point P1, Point P2) {
-    Point R;
+    Point P3;
 
     int dx = P1.x ^ P2.x;
     int dy = P1.y ^ P2.y;
@@ -87,20 +87,39 @@ Point addition(Point P1, Point P2) {
         }
         else {
             // x좌표만 같을 때 -> point of infinity
-            R.x = 0;
-            R.y = 0;
-            return R;
+            P3.x = 0;
+            P3.y = 0;
+            return P3;
         }
     }
     else {
         int dx_inv = gf_inverse[dx];
         int lambda = gf_mul(dy, dx_inv);
 
-        R.x = (gf_mul(lambda, lambda)) ^ lambda ^ P1.x ^ P2.x ^ A;
-        R.y = (gf_mul(lambda, P1.x ^ R.x)) ^ R.x ^ P1.y;
+        P3.x = (gf_mul(lambda, lambda)) ^ lambda ^ P1.x ^ P2.x ^ A;
+        P3.y = (gf_mul(lambda, P1.x ^ P3.x)) ^ P3.x ^ P1.y;
     }
 
-    return R;
+    return P3;
+}
+
+// Define Scalar Multiplication (Left-to-Right method)
+Point multiplication(Point P, int d) {
+    Point Result = P;
+
+    while (d > 0) {
+        Result = doubling(Result);
+        
+        // If d_i == 1, operate addition
+        if ((d & 1) == 1) {
+            Result = addition(Result, P);
+        }
+        
+        // Right bit shift
+        d = d >> 1;
+    }
+
+    return Result;
 }
 
 // Print as binary
@@ -108,6 +127,15 @@ void print_bin(int num) {
 	for (int cnt_i = 3; cnt_i >= 0; cnt_i--) {
 		printf("%d", (num >> cnt_i) & 1);
 	}
+}
+
+// Print as point coordinates
+void print_point(Point point) {
+    printf("(");
+    print_bin(point.x);
+    printf(", ");
+    print_bin(point.y);
+    printf(")");
 }
 
 // Print table 
@@ -120,21 +148,39 @@ void print_table(Point points[], int size)
 		for (int cnt_j = 0; cnt_j < size; cnt_j++) {
             result = addition(points[cnt_i], points[cnt_j]);
 
-            printf("(");
-            print_bin(points[cnt_i].x);
-            printf(", ");
-            print_bin(points[cnt_i].y);
-            printf(") + (");
-            print_bin(points[cnt_j].x);
-            printf(", ");
-            print_bin(points[cnt_j].y);
-            printf(") = (");
-            print_bin(result.x);
-            printf(", ");
-            print_bin(result.y);
-            printf(")\n");
+            print_point(points[cnt_i]);
+            printf(" + ");
+            print_point(points[cnt_j]);
+            printf(" = ");
+            print_point(result);
+            printf("\n");
 		}
 	}
+}
+
+// Check E(F_2^4) is cyclic
+void check_cyclic(Point alpha) {
+    Point original = alpha;
+
+    printf("[0 multiplication] = point of infinity\n");
+    printf("[%d multiplication] = ", 1);
+    print_point(alpha);
+
+    printf("\n[%d multiplication] = ", 2);
+    alpha = doubling(alpha);
+    print_point(alpha);
+
+    // 0P, 1P, 2P는 이미 출력했으니 3P부터 출력
+    for (int cnt_i = 3; cnt_i < 21; cnt_i++) {
+        printf("\n[%d multiplication] = ", cnt_i);
+        alpha = addition(alpha, original);
+        print_point(alpha);
+
+        // Point of infinity인 경우
+        if (alpha.x == 0b0000 && alpha.y == 0b0000) {
+            printf(" ====> point of infinity\n\n");
+        }
+    }
 }
 
 int main() {
@@ -150,7 +196,10 @@ int main() {
     };
 
 	int size = sizeof(points) / sizeof(points[0]);
+    print_table(points, size);
+    
+    Point P = { 0b1000, 0b0001 };
+    check_cyclic(P);
 
-	print_table(points, size);
 	return 0;
 }
